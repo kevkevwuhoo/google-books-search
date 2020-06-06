c2f309d3688809912db03ae87818c38434d96784
kwu3 <kwu3@villanova.edu>
2020-06-05 22:56:15 -0400

    Fixed nav to add Search and Saved tabs

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,10 +2,18 @@ import React from "react";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav className="navbar navbar-expand navbar-dark bg-primary">
       <a className="navbar-brand" href="/">
-        React Reading List
+        Google Books Search
       </a>
+      <div className="navbar-nav">
+        <a className="nav-item nav-link active" href="/search">
+          Search
+        </a>
+        <a className="nav-item nav-link" href="/saved">
+          Saved
+        </a>
+      </div>
     </nav>
   );
 }
