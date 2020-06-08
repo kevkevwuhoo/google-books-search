6a01829ab2dad383d184d32857d9e464b9fe485e
kwu3 <kwu3@villanova.edu>
2020-06-07 22:36:45 -0400

    Changed route components to render pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,19 @@
 import React from "react";
-import Search from "./pages/Search";
-import NoMatch from "./pages/NoMatch";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
 
-// The app will not render correctly until you setup a Route component.
-// Refer to the Basic Example documentation if you need to.
-// (https://reacttraining.com/react-router/web/example/basic)
 function App() {
   return (
-    <BrowserRouter>
-      <div>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={Search} />
-          <Route exact path="/books" component={Search} />
-          {/* <Route path="/books/:id" component={} /> */}
-          <Route path="*" component={NoMatch} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <Router>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Search} />
+        <Route exact path="/search" component={Search} />
+        <Route exact path="/saved" component={Saved} />
+      </Switch>
+    </Router>
   );
 }
 
