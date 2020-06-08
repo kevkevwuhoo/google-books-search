bdec7ef55f886bbb0eb1d1139cb8fb68c05177fe
kwu3 <kwu3@villanova.edu>
2020-06-07 22:33:54 -0400

    Changed nav items to link components

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,21 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand navbar-dark bg-primary">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <a className="navbar-brand" href="/">
         Google Books Search
       </a>
-      <div className="navbar-nav">
-        <a className="nav-item nav-link active" href="/search">
-          Search
-        </a>
-        <a className="nav-item nav-link" href="/saved">
-          Saved
-        </a>
-      </div>
+      <Link style={styles.link} to="/search">
+        Search
+      </Link>
+      <Link style={styles.link} to="/saved">
+        Saved
+      </Link>
     </nav>
   );
 }
 
 export default Nav;
+
+const styles = {
+  link: {
+    color: "white",
+    padding: "5px",
+  },
+};
